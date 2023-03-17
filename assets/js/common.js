39ad3402dbec540d854ae6f80b5a53692f83a531
PROLOVE <james212.dev@gmail.com>
2023-03-17 12:49:42 +0900

    begin to make logic

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,33 +2,34 @@ var store = {}
 var prefectures = []
 var q1;
 
+// loadPrefecture();
+
 function removeActive(param){
     $(param).removeClass("is-active")
 }
 
+function hideSection(st){
+    var cnt_max = $(".p-select").length;
+    for (let index = st; index <= cnt_max; index++) {
+        const element = $(`#card-${index}`);
+        element.addClass("d-none");
+    }
+}
 
 $(".js-q1").click(function(){
     removeActive(".js-q1");
+    hideSection(3);
+    $(`#card-2`).removeClass("d-none")
     $(this).addClass("is-active");
     q1 = $(this).children("p").html();
 })
 
-$(".js-q2").click(function(){
-    console.log("asdf")
-    removeActive(".js-q2");
-    $(this).addClass("is-active");
-
-    var name = $(this).attr("value");
-
-    console.log(name)
-    loadData(name)
-    
-})
 
 $(".js-q3").click(function(){
     removeActive(".js-q3");
     $(this).addClass("is-active");
     q2 = $(this).children("p").html();
+
 })
 
 $(".js-q4").click(function(){
@@ -53,83 +54,3 @@ $(".js-q7").click(function(){
 $("#city_selector").change(function(){
     refreshVillage($("#city_selector").val())
 })
-
-
-function refreshVillage(city_name){
-    data = Object.keys(store[city_name])
-
-    $("#village_selector").html("")
-
-    data.forEach(village =>{
-        $("#village_selector").append("<option value='"+village+"'>"+village+"</option>")
-    })
-}
-
-function loadData(name) {
-    fetch('/assets/data/'+name+".xlsx", {
-        method: 'get',
-    }).then(res => { 
-        return res.arrayBuffer();
-    })
-    .then(res => {
-        var workbook = XLSX.read(new Uint8Array(res), {
-            type: 'array'
-        });
-
-        var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets["Sheet1"]);
-
-        store = {}
-
-        $("#city_selector").html("")
-        $("#village_selector").html("")
-
-        XL_row_object.forEach(function(row){
-            if(store[row['市区町村']]){
-                store[row['市区町村']][row['町字']] = {
-                    "最低土地値" : row['最低土地値'],
-                    "最高土地値" : row['最高土地値']
-                }
-            }else{
-                store[row['市区町村']] = {}
-                $("#city_selector").append("<option value='"+row['市区町村']+"'>"+row['市区町村']+"</option>")
-            }
-        })
-
-        if(Object.keys(store).length > 0){
-            refreshVillage(Object.keys(store)[0])
-        }else{
-            $("#village_selector").html("")
-        }
-    });
-}
-
-
-function loadPrefecture() {
-    fetch('/assets/data/index.xlsx', {
-        method: 'get',
-    }).then(res => { 
-        return res.arrayBuffer();
-    })
-    .then(res => {
-        var workbook = XLSX.read(new Uint8Array(res), {
-            type: 'array'
-        });
-
-        var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets["Sheet1"]);
-
-        prefectures = []
-
-        $("#prefecture_selector").html("")
-
-        XL_row_object.forEach(function(row){
-            if(prefectures.length == 0){
-                $("#prefecture_selector").append("<li class='p-select__item js-q2 is-active' value='"+row['都道府県']+"' ><p>" + row['都道府県'] + "</p></li>")
-            }else{
-                $("#prefecture_selector").append("<li class='p-select__item js-q2' value='"+row['都道府県']+"' ><p>" + row['都道府県'] + "</p></li>")
-            }
-            prefectures.push(row['都道府県'])
-        })
-        
-    });
-}
-
--- assets/js/import.js
+++ assets/js/import.js
@@ -1,31 +1,92 @@
-var ExcelToJSON = function() {
-    var workbook = XLSX.read(data, {
-      type: 'binary'
-    });
-    workbook.SheetNames.forEach(function(sheetName) {
-      var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-      var productList = JSON.parse(JSON.stringify(XL_row_object));
 
-      var rows = $('#tblItems tbody');
-      // console.log(productList)
-      for (i = 0; i < productList.length; i++) {
-        var columns = Object.values(productList[i])
-        rows.append(`
-            <tr>
-                <td>${columns[0]}</td>
-                <td>${columns[1]}</td>
-                <td>${columns[2]}</td>
-                <td>${columns[3]}</td>
-                <td>${columns[4]}</td>
-            </tr>
-        `);
-      }
+function refreshVillage(city_name){
+    data = Object.keys(store[city_name])
 
+    $("#village_selector").html("")
+
+    data.forEach(village =>{
+        $("#village_selector").append("<option value='"+village+"'>"+village+"</option>")
     })
-  };
-  
-  function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
-    var xl2json = new ExcelToJSON();
-    xl2json.parseExcel(files[0]);
-  }
+}
+
+function loadData(name) {
+    fetch('/assets/data/'+name+".xlsx", {
+        method: 'get',
+    }).then(res => { 
+        return res.arrayBuffer();
+    })
+    .then(res => {
+        var workbook = XLSX.read(new Uint8Array(res), {
+            type: 'array'
+        });
+
+        var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets["Sheet1"]);
+
+        store = {}
+
+        $("#city_selector").html("")
+        $("#village_selector").html("")
+
+        XL_row_object.forEach(function(row){
+            if(store[row['市区町村']]){
+                store[row['市区町村']][row['町字']] = {
+                    "最低土地値" : row['最低土地値'],
+                    "最高土地値" : row['最高土地値']
+                }
+            }else{
+                store[row['市区町村']] = {}
+                $("#city_selector").append("<option value='"+row['市区町村']+"'>"+row['市区町村']+"</option>")
+            }
+        })
+
+        if(Object.keys(store).length > 0){
+            refreshVillage(Object.keys(store)[0])
+        }else{
+            $("#village_selector").html("")
+        }
+    });
+}
+
+
+function loadPrefecture() {
+    fetch('/assets/data/index.xlsx', {
+        method: 'get',
+    }).then(res => { 
+        return res.arrayBuffer();
+    })
+    .then(res => {
+        var workbook = XLSX.read(new Uint8Array(res), {
+            type: 'array'
+        });
+
+        var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets["Sheet1"]);
+
+        prefectures = []
+
+        $("#prefecture_selector").html("")
+
+        XL_row_object.forEach(function(row){
+            if(prefectures.length == 0){
+                $("#prefecture_selector").append("<li class='p-select__item js-q2 is-active' data-value='"+row['都道府県']+"' ><p>" + row['都道府県'] + "</p></li>")
+                
+                loadData(row['都道府県'])
+            }else{
+                $("#prefecture_selector").append("<li class='p-select__item js-q2' data-value='"+row['都道府県']+"' ><p>" + row['都道府県'] + "</p></li>")
+            }
+            prefectures.push(row['都道府県'])
+        })
+
+        
+        $(".js-q2").click(function(){
+            removeActive(".js-q2");
+            $(this).addClass("is-active");
+                
+            hideSection(4);
+            $(`#card-3`).removeClass("d-none")
+            var name = $(this).data("value");        
+            loadData(name)
+        })
+    });
+}
+
+loadPrefecture() ;
